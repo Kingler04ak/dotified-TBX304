6505c5ec2915b60624b15baebbd36891a52917d7
Akash23q2 <Akash23q2@gmail.com>
2021-07-03 21:41:46 +0530

    viper-os

--- viper_TBX304.mk
+++ viper_TBX304.mk
@@ -28,13 +28,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/TBX304/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_TBX304
+PRODUCT_NAME := viper_TBX304
 PRODUCT_DEVICE := TBX304
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo TB-X304L
